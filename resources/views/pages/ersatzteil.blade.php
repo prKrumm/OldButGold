0001bc70223b4753de2a4019105d4b8ec69cf063
prKrumm <pr.krumm@web.de>
2017-04-13 10:55:29 +0200

    teile anfragen in blade

--- resources/views/pages/ersatzteil.blade.php
+++ resources/views/pages/ersatzteil.blade.php
@@ -1,35 +1,42 @@
 @extends('layouts.masterFahrzeug')
 
 @section('content')
-    <div class="container">
-        <div class="col-md-9">
-            <div>
-                <p> Lorem IPSUM</p>
-            </div>
-            <div>
-                <p> Lorem IPSUM</p>
-            </div>
-            <div>
-                <p> Lorem IPSUM</p>
-            </div>
-            <div>
-                <p> Lorem IPSUM</p>
-            </div>
-        </div>
+    <h1>Ersatzteil Anfragen</h1>
 
-        <div class="col-md-3">
-            <div>
-                <p> Lorem IPSUM</p>
+
+    <div id="qlist-wrapper">
+        <div class="container">
+            <div class="col-md-3">
+                <div class="question-summary narrow"
+                     id="question-summary-42972375">
+
+                    <div class="votes">
+                        <div class="mini-counts"><span title="0 votes">0</span></div>
+                        <div>votes</div>
+                    </div>
+
+                    <div class="status unanswered">
+                        <div class="mini-counts"><span title="0 answers">0</span></div>
+                        <div>answers</div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <p> Lorem IPSUM</p>
-            </div>
-            <div>
-                <p> Lorem IPSUM</p>
-            </div>
-            <div>
-                <p> Lorem IPSUM</p>
+            <div class="col-md-9">
+                <div class="summary">
+
+                    <h3><a href="/questions/42972375/parse-integer-and-show-in-timeline-in-kibana"
+                           class="question-hyperlink">Parse integer and show in timeline in Kibana</a></h3>
+                    <div class="tags t-kibana t-kibana-5">
+                        <a href="/questions/tagged/kibana" class="post-tag"
+                           title="show questions tagged &#39;kibana&#39;" rel="tag">kibana</a> <a
+                                href="/questions/tagged/kibana-5" class="post-tag"
+                                title="show questions tagged &#39;kibana-5&#39;" rel="tag">kibana-5</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
+    
+
 @endsection
