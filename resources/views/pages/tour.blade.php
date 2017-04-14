4690888ab47cf257285dea8c5242012735a4aaba
Viktoria1 <Viktoria.m.schreiner@gmail.com>
2017-04-14 12:42:11 +0200

    some changes for welcome, tour

--- resources/views/pages/tour.blade.php
+++ resources/views/pages/tour.blade.php
@@ -2,22 +2,80 @@
 @section('content')
 
     <div class="container">
-       ICH WERDE VON HEADER ÜBERDECKT
+        <div class="text-center">
+            <h1> Tour</h1>
+            <span>Willkommen auf dem Oldtimer - Portal</span>
+            <hr>
+        </div>
+        <h3>Treffpunkt: Stelle Fragen, erhalten Antworten - simpel und effektiv</h3>
+        <br>
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <img alt="Image" class="img-thumbnail" src="../images/landingpage.jpg">
+            <div class="col-md-7">
+
+                <span>Dies ist ein Typoblindtext. An ihm kann man sehen,
+                    ob alle Buchstaben da sind und wie sie aussehen. Manchmal
+                    benutzt man Worte wie Hamburgefonts, Rafgenduks oder Handgloves,
+                    um Schriften zu testen. Manchmal Sätze, die alle Buchstaben des
+                    Alphabets enthalten - man nennt diese Sätze »Pangrams«. Sehr
+                    bekannt ist dieser: The quick brown fox jumps over the lazy old
+                    dog. Oft werden in Typoblindtexte auch fremdsprachige Satzteile
+                    eingebaut (AVAIL® and Wefox™ are testing aussi la Kerning), um
+                    die Wirkung in anderen Sprachen zu testen. In Lateinisch sieht zum
+                    Beispiel fast jede Schrift gut aus. Quod erat demonstrandum. Seit
+                    1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach
+                    TypoGb. 204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht
+                    werden. Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft.
+                    Genauso wichtig in sind mittlerweile auch Âçcèñtë, die in neueren
+                    Schriften aber fast immer enthalten sind. Ein wichtiges aber
+                    schwierig zu integrierendes Feld sind OpenType-Funktionalitäten.
+                    Je nach Software und Voreinstellungen können eingebaute Kapitälchen,
+                    Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt
+                    werden.Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle
+                    Buchstaben da sind und wie sie aussehen.  </span>
+            </div>
+            <div class="col-md-5">
+                <img alt="Image" class="img-thumbnail" src="../images/Cadillac.jpg">
             </div>
         </div>
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="text-center">
-                    Lorem IPSUM
-                    Lorem IPSUM
-                    Lorem IPSUMLorem IPSUMLorem IPSUMLorem IPSUM
-                    Lorem IPSUM
-                    Lorem IPSUMLorem IPSUMLorem IPSUMLorem IPSUMLorem IPSUMLorem IPSUMLorem IPSUMLorem IPSUM
+            <h3>Ersatzteile: Wer suchet der findet</h3>
+            <div class="col-md-7">
+                <span>
+                    auf sich warten. Damit das Layout nun nicht nackt im Raume steht und
+                    sich klein und leer vorkommt, springe ich ein: der Blindtext. Genau zu
+                    diesem Zwecke erschaffen, immer im Schatten meines großen Bruders
+                    »Lorem Ipsum«, freue ich mich jedes Mal, wenn Sie ein paar Zeilen lesen.
+                    Denn esse est percipi - Sein ist wahrgenommen werden. Und weil Sie nun
+                    schon die Güte haben, mich ein paar weitere Sätze lang zu begleiten,
+                    möchte ich diese Gelegenheit nutzen, Ihnen nicht nur als Lückenfüller zu
+                    dienen, sondern auf etwas hinzuweisen, das es ebenso verdient wahrgenommen
+                    zu werden: Webstandards nämlich. Sehen Sie, Webstandards sind das Regelwerk,
+                    auf dem Webseiten aufbauen. So gibt es Regeln für HTML, CSS, JavaScript oder
+                    auch XML; Worte, die Sie vielleicht schon einmal von Ihrem Entwickler gehört
+                    haben. Diese Standards sorgen dafür, dass alle Beteiligten aus einer Webseite
+                    den größten Nutzen ziehen. Im Gegensatz zu früheren Webseiten müssen wir zum
+                    Beispiel nicht mehr zwei verschiedene Webseiten für den Internet Explorer
+                    und einen anderen Browser programmieren. Es reicht eine Seite, die - richtig
+                    angelegt - sowohl auf verschiedenen Browsern im Netz funktioniert, aber ebenso
+                    gut für den Ausdruck oder
+                </span>
+            </div>
+            <div class="col-md-5">
+                <img alt="Image" class="img-thumbnail" src="../images/ersatzteile.jpg">
+            </div>
+        </div>
 
-                </div>
+        <div class="row">
+            <h3>Experten: Finde Experten in deiner Nähe</h3>
+            <div class="col-md-7">
+                <span>
+                     Ein wichtiges aber
+                    schwierig zu integrierendes Feld sind OpenType-Funktionalitäten.
+                    Je nach Software und Voreinstellungen können eingebaute Kapitälchen,
+                    Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt
+                    werden.Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle
+                    Buchstaben da sind und wie sie aussehen. </span>
+
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
+@section('content')
 
-        <title>Laravel</title>
+    <div class="container">
+        <br>ICH WERDE ÜBERDECKT
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <img alt="Image" class="img-thumbnail" src="../images/landingpage.jpg">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="text-muted">
+                    Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte.
+                    Abgeschieden wohnen Sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein
+                    kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien.
+                    Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht
+                    einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht - ein geradezu
+                    unorthographisches Leben. Eines Tages aber beschloß eine kleine Zeile Blindtext, ihr Name war Lorem
+                    Ipsum, hinaus zu gehen in die weite Grammatik. Der große Oxmox riet ihr davon ab, da es dort wimmele
+                    von bösen Kommata, wilden Fragezeichen und hinterhältigen Semikoli, doch das Blindtextchen ließ sich
+                    nicht beirren. Es packte seine sieben Versalien, schob sich sein Initial in den Gürtel und machte
+                    sich auf den Weg. Als es die ersten Hügel des Kursivgebirges erklommen hatte, warf es einen letzten
+                    Blick zurück auf die Skyline seiner Heimatstadt Buchstabhausen, die Headline von Alphabetdorf und
+                    die Subline seiner eigenen Straße, der Zeilengasse.
+                    <br><br>
+                    Starte mit einem kurzen Überblick über das Portal. Link zur <a href="/tour">Tour</a>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
