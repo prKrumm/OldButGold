393d33087fdb2d5431c4e9e8016935479d5489ea
Viktoria1 <Viktoria.m.schreiner@gmail.com>
2017-04-20 13:11:39 +0200

    added text Überuns

--- resources/views/pages/ueberuns.blade.php
+++ resources/views/pages/ueberuns.blade.php
@@ -2,7 +2,33 @@
 
 @section('content')
     <div class="container">
-        <h2 class="left">Über uns</h2>
+        <div class="row">
+            <h2 class="left">Über uns</h2>
+
+            <div class="col-md-4">
+                <img alt="Image" class="img-thumbnail" src="../images/liebhaber.jpg">
+            </div>
+
+            <div class="col-md-8 col-xs-8">
+
+                <section>
+                    Wir sind selbst Oldtimer Liebhaber und stellten in den vergangenen Jahren immer wieder fest, dass
+                    trotz der großen Schrauber- und Sammlerszene in Deutschland ein Portal fehlt, welche die
+                    Gemeinschaft - das sind Experten, Restaurateure und Händler - besser vernetzt.
+
+                    Aus diesem Grund bieten wir dieses Portal an. Es erfüllt zwei Aufgaben.<br><br>
+                    <ul>
+                        <li>Fragen stellen und qualifizierte Antworten zu erhalten</li>
+                        <li>Ersatzteile zu suchen und Händler zu finden</li>
+                    </ul>
+                    <br>
+
+                    Die Nutzung der Plattform soll dabei
+                    <span class="text-success">   nicht-kommerziell und werbefrei bleiben!</span>
+                </section>
+
+            </div>
+        </div>
     </div>
 
 @endsection
