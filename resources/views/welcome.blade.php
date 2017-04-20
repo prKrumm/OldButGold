d6c01e2238710075d68ca0adc289e805501c745a
Viktoria1 <Viktoria.m.schreiner@gmail.com>
2017-04-20 14:30:09 +0200

    added text for profil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,28 +3,17 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2">
+            <br><br>
+            <div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
                 <img alt="Image" class="img-thumbnail" src="../images/landing.jpg">
             </div>
         </div>
         <div class="row">
-            <div class="col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2" >
+            <div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 center" >
                 <div class="text-muted">
-                    Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte.
-                    Abgeschieden wohnen Sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein
-                    kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien.
-                    Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht
-                    einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht - ein geradezu
-                    unorthographisches Leben. Eines Tages aber beschloß eine kleine Zeile Blindtext, ihr Name war Lorem
-                    Ipsum, hinaus zu gehen in die weite Grammatik. Der große Oxmox riet ihr davon ab, da es dort wimmele
-                    von bösen Kommata, wilden Fragezeichen und hinterhältigen Semikoli, doch das Blindtextchen ließ sich
-                    nicht beirren. Es packte seine sieben Versalien, schob sich sein Initial in den Gürtel und machte
-                    sich auf den Weg. Als es die ersten Hügel des Kursivgebirges erklommen hatte, warf es einen letzten
-                    Blick zurück auf die Skyline seiner Heimatstadt Buchstabhausen, die Headline von Alphabetdorf und
-                    die Subline seiner eigenen Straße, der Zeilengasse.
+                    Faszination Oldtimer – Das Oldtimer Portal Old-But-Gold bringt Oldtimer-Liebhaber, Experten, Restaurateure und Händler zusammen.
                     <br><br>
-                    Starte mit einem kurzen Überblick über das Portal. Link zur <a href="/tour">Tour</a>
-
+                    Finde hier mehr über Old-but-Gold heraus und starte mit der <a href="/tour">Tour</a>.
                 </div>
             </div>
         </div>
