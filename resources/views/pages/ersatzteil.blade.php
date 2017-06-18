bd863e656149be8ed408b247dd8fae48da9dfa97
Florian Kaiser <floriankaiser@Florians-MacBook-Pro.local>
2017-06-18 10:36:33 +0200

    nichts besonderes

--- resources/views/pages/ersatzteil.blade.php
+++ resources/views/pages/ersatzteil.blade.php
@@ -13,11 +13,11 @@
 
     <div class="fragenCon">
     <div class="row">
-        <div class="col-md-9 col-sm-9">
+        <div class="col-md-9 col-sm-8">
             <h2 class="left">Alle Gesuche  <span class="badge">{{$fragen->total()}}</span></h2>
         </div>
-        <div class="col-md-3 col-sm-3">
-            <a class="btn btn-default" href="/ersatzteil/create" id="frageKnopf">Teil anfragen</a>
+        <div class="col-md-3 col-sm-4">
+            <a class="btn btn-default" href="/ersatzteil/create" id="frageKnopf"><span class="glyphicon glyphicon-wrench"></span> Teil anfragen</a>
         </div>
     </div>
 
--- resources/views/pages/treffpunkt.blade.php
+++ resources/views/pages/treffpunkt.blade.php
@@ -13,11 +13,11 @@
 
     <div class="fragenCon">
     <div class="row">
-        <div class="col-md-9 col-sm-9">
+        <div class="col-md-9 col-sm-8">
             <h2 class="left">Alle Fragen <span class="badge">{{$fragen->total()}}</span></h2>
         </div>
-        <div class="col-md-3 col-sm-3">
-            <a class="btn btn-default" href="/treffpunkt/create" id="frageKnopf">Frage stellen</a>
+        <div class="col-md-3 col-sm-4">
+            <a class="btn btn-default" href="/treffpunkt/create" id="frageKnopf"><span class="glyphicon glyphicon-comment"></span> Frage stellen</a>
         </div>
     </div>
 
