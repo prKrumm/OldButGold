6002cb2f94627263a0cd99cfad28e29b7507456f
Viktoria1 <Viktoria.m.schreiner@gmail.com>
2017-06-22 19:34:08 +0200

    little SEO

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -8,12 +8,16 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<!--SEO -SERP-->
+<meta name="description"
+      content="Faszination Oldtimer – Das Oldtimer Portal Old-But-Gold bringt Oldtimer-Liebhaber, Experten, Restaurateure und Händler zusammen. Experten,...">
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans"/>
 
-<link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap.tagsinput/0.4.2/bootstrap-tagsinput.css" />
+<link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap.tagsinput/0.4.2/bootstrap-tagsinput.css"/>
 <script src="//cdn.jsdelivr.net/bootstrap.tagsinput/0.4.2/bootstrap-tagsinput.min.js"></script>
 <!--AJAX JQuery-->
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
