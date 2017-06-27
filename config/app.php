ebf15fffb9ff17a4006b38a6ef6f243fc01561b8
PatrickK <patrick.krumm@htwg-konstanz.de>
2017-06-27 20:44:50 +0200

    removed deprecated dependency

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
